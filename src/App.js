6175b40402aa4f973f7780309768347a98d31443
fatihsahin291 <musabsahin291@gmail.com>
2023-11-08 14:22:38 +0300

    added useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,57 @@
 import Main from "./Main";
 import Header from "./Header";
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+	questions: [],
+	// 'loading' | 'error' | 'ready' | 'active' | 'finished'
+	status: "loading",
+};
+
+function reducer(state, action) {
+	switch (action.type) {
+		case "dataReceived":
+			return {
+				...state,
+				questions: action.payload,
+				status: "ready",
+			};
+
+		case "dataFailed":
+			return {
+				...state,
+				status: "error",
+			};
+
+		default:
+			throw new Error("Action unknown");
+	}
+}
 
 export default function App() {
+	const [state, dispatch] = useReducer(
+		reducer,
+		initialState
+	);
+
+	useEffect(() => {
+		fetch("http://localhost:5000/questions")
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				dispatch({
+					type: "dataReceived",
+					payload: data,
+				});
+			})
+			.catch((error) => {
+				dispatch({
+					type: "dataFailed",
+				});
+			});
+	}, []);
+
 	return (
 		<div className="app">
 			<Header />
